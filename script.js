cffe8ddb5b646ffbccf08dd9b2f54dbda63cf8ba
skantrek <adriansaurio@gmail.com>
2024-07-22 21:42:23 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.getElementById('searchBar').addEventListener('keyup', function() {
+    let query = this.value.toLowerCase();
+    let products = document.querySelectorAll('.product');
 
+    products.forEach(function(product) {
+        let productName = product.getAttribute('data-name').toLowerCase();
+        if (productName.includes(query)) {
+            product.style.display = 'block';
+        } else {
+            product.style.display = 'none';
+        }
+    });
+});
